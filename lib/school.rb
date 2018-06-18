d63bd74daebcf025738dc3a9475a1ef5fa7a863e
Kevin Chi <kevinchi118@gmail.com>
2018-06-18 22:04:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
         @roster = {}
     end
 
-    def add_student(Student, grade)
+    def add_student(student, grade)
         @roster[grade] ? @roster[grade] << Student : @roster[grade] = [student]
     end
 end
