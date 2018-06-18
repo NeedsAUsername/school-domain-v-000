9464dda56545736780a476525307c34447691c00
Kevin Chi <kevinchi118@gmail.com>
2018-06-18 22:01:29 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,7 @@ class School
     end
 
     def add_student(student, grade)
-        if @roster[grade]
-            @roster[grade] << student
-        else @roster[grade] = [student]
-        end
+        @roster[grade] ? @roster[grade] << student : @roster[grade] = [student]
     end
 
     def grade(grade)
