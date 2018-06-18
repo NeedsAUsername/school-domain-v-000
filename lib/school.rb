5fe5d39ef9b3b3800ec2d01ca150b1171b911682
Kevin Chi <kevinchi118@gmail.com>
2018-06-18 22:02:11 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,4 @@
 class School
 
-    attr_accessor :name, :roster
-
-    def initialize(name)
-        @name = name
-        @roster = {}
-    end
-
-    def add_student(student, grade)
-        @roster[grade] ? @roster[grade] << student : @roster[grade] = [student]
-    end
-
-    def grade(grade)
-        @roster.collect do |grades, students|
-            if grades == grade
-                students
-            end
-        end
-
-    def sort
-        @roster.collect do |grades, students|
-            students.sort
-        end
-    end
 
 end
