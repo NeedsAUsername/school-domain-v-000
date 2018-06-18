2a0b65a3c75019a7dd3fee6e43864564ac71a162
Kevin Chi <kevinchi118@gmail.com>
2018-06-18 22:04:01 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,11 @@ class School
 
     def initialize(name, grade)
         @name = name
-        @grade = {}
+        @roster = {}
+    end
+
+    def add_student(Student, grade)
+        if @roster[grade] ? @roster[grade] << Student : @roster[grade] = [student]
+        end
     end 
 end
