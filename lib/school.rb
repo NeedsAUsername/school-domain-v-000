a255cbd9986ad0587b5c7eadfa864280bde32769
Kevin Chi <kevinchi118@gmail.com>
2018-06-18 21:52:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,11 +7,22 @@ class School
     end
 
     def add_student(student, grade)
-        if roster[grade]
-            
-
+        if @roster[grade]
+            @roster[grade] << student
+        else @roster[grade] = student
     end
 
+    def grade(grade)
+        @roster.collect do |grades, students|
+            if grades == grade
+                students
+            end
+        end
+
+    def sort
+        @roster.collect do |grades, students|
+            students.sort
+        end
 end
 
 school = School.new("example")
